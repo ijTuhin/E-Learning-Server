b3fe7aa93b6e3b015da4917cd9b9edc82f43e74c
ijTuhin <ijTuhin638@gmail.com>
2022-11-28 21:19:06 -0800

    DB connected

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // using middleware
 app.use(cors());
 app.use(express.json());
+
+// userName: dbuser
+// password: s5qpYxyO6JvdKBk1
+
+
+const uri = "mongodb+srv://dbuser:s5qpYxyO6JvdKBk1@cluster0.hfup90w.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('DB connected');
+  // perform actions on the collection object
+  client.close();
+});
+
 
 
 app.get('/', (req,res) => {
